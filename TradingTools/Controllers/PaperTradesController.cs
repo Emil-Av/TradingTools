5e766f1b835d728ac9dcbeabf399fa30d014e5f9
Emil <Test@DESKTOP-7VQJOGD>
2024-04-17 11:42:29 +0200

    Side- and topbar integrated as partial view for all pages.

---
+++ TradingTools/Controllers/PaperTradesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradingTools.Controllers
+{
+    public class PaperTradesController : Controller
+    {
+        public PaperTradesController() 
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
