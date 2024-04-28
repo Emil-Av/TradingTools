809b46481eb365b34abd4e9bf7d37aee0ed4e9c3
Emil <emil.arnaudov@yahoo.com>
2024-04-28 19:53:20 +0200

    Trade Model

---
+++ Models/Trade.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Trade
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Instrument { get; set; }
+
+
+        public double? TriggerPrice { get; set; }
+
+        [Required]
+        public double? EntryPrice { get; set; }
+
+        [Required]
+        public double? StopPrice { get; set; }
+
+        [Required]
+
+        public double? FirstTarget { get; set; }
+
+        public double? SecondTarget { get; set; }
+
+        public double? ExitPrice { get; set; }
+
+        public double? Profit { get; set; }
+
+        public double? Loss { get; set; }
+
+        public double? Fee { get; set; }
+
+        public enum TimeFrame { M5, M10, M15, M30, H1, H2, H4, D}
+
+        public enum Status { Active, Closed, Pending, Cancelled }
+
+        public enum Strategy { FirstBarBelowAbove, Cradle }
+
+        public enum SideType { Long, Short }
+
+        public enum OrderType { Limit, Market}
+
+        public DateTime? EntryTime { get; set; }
+
+        public DateTime? ExitTime { get; set; }
+
+        public DateTime? TradeDuration { get; set; }
+    }
+}
