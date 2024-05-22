01bbd4caa298994737eba3fd869e32bfdafe9733
Emil Arnaudov <emil.arnaudov@yahoo.com>
2024-05-22 17:58:29 +0200

    Calculator table

--- TradingTools/wwwroot/js/site.js
+++ TradingTools/wwwroot/js/site.js
@@ -1,15 +1,14 @@
-﻿$(document).click(function (event) {
-    // Hide the menu if it's clicked outside of it
-    $(document).on('click', function (event) {
-        var menu = $('#menuTrades');
-        var target = $(event.target);
-        var isClickInsideMenu = target.closest('#menuTrades').length > 0;
+﻿
+// Hide the menu if it's clicked outside of it
+$(document).on('click', function (event) {
+    var menu = $('#menuTrades');
+    var target = $(event.target);
+    var isClickInsideMenu = target.closest('#menuTrades').length > 0;
 
-        if (!isClickInsideMenu && menu.hasClass('show')) {
-            // If the click is outside of the menu and the menu is open, collapse it
-            menu.removeClass('show');
-        }
-    });
+    if (!isClickInsideMenu && menu.hasClass('show')) {
+        // If the click is outside of the menu and the menu is open, collapse it
+        menu.removeClass('show');
+    }
 });
 
 
