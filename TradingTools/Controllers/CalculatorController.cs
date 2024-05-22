7d00faa46205e2599bd6656f26abe53fb87b4c85
Emil Arnaudov <emil.arnaudov@yahoo.com>
2024-05-22 10:26:29 +0200

    Trade calculator html

---
+++ TradingTools/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradingTools.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
