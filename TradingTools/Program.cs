294d9ecca280601c6d22b8ec12c04cfb05692654
emila <emil.arnaudov@yahoo.com>
2025-04-03 20:28:23 +0200

    Test

--- TradingTools/Program.cs
+++ TradingTools/Program.cs
@@ -6,7 +6,7 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container .
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
